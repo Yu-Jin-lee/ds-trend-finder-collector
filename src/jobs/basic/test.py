d698325159f621bac1bb42dda004870f6c3c1eb6
Yu-Jin-lee <yujin.lee@ascentnet.co.jp>
2024-11-11 18:23:30 +0900

    테스트 코드 삭제

--- src/jobs/basic/test.py
+++
@@ -1,6 +0,0 @@
-from jobs.basic.get_serp import EntitySerpDaily
-from jobs.basic.get_suggest import EntitySuggestDaily
-
-# test = EntitySerpDaily("2024111116", "ko", "google", "False")
-test = EntitySuggestDaily("ko", "google")
-test.test()
